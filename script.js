bf79c629b41a5b8240c92b432934758eadbe2c68
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-27 10:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function isFibonacci(num) {
+function isFibonacci(n) {
 		// Write your code here
+	 function isPerfectSquare(x) {
+        let s = Math.sqrt(x);
+        return s === Math.floor(s);
+    }
+
+    // Check the Fibonacci property
+    const condition1 = 5 * n * n + 4;
+    const condition2 = 5 * n * n - 4;
+
+    // Return true if either condition is a perfect square
+    return isPerfectSquare(condition1) || isPerfectSquare(condition2);
 }
-const input = parseInt(prompt("Enter a number"));
-alert(isFibonacci(input));
+// const input = parseInt(prompt("Enter a number"));
+// alert(isFibonacci(input));
